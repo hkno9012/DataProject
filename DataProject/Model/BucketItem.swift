52ed85f90bac2cd14c42e802e1d3786ccfdff5ac
HunterKnott <hunterknott00@gmail.com>
2018-11-30 12:06:49 -0700

    Added initial files

---
+++ DataProject/Model/BucketItem.swift
@@ -0,0 +1,21 @@
+//
+//  BucketItem.swift
+//  DataProject
+//
+//  Created by Knott, Hunter on 11/30/18.
+//  Copyright © 2018 Knott, Hunter. All rights reserved.
+//
+
+import Foundation
+
+public class BucketItem
+{
+    let itemContents : String
+    let itemAuthor : String
+    
+    init(contents : String, author : String)
+    {
+        itemContents = contents
+        itemAuthor = author
+    }
+}
