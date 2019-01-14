2948c55797b54feb945fc8f4312c940ddf38d86d
HunterKnott <hunterknott00@gmail.com>
2019-01-14 12:43:21 -0700

    Got the file to load onto the screen

--- DataProject/Controller/DataViewController.swift
+++ DataProject/Controller/DataViewController.swift
@@ -10,10 +10,11 @@ import UIKit
 
 class DataViewController: UITableViewController
 {
-
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -32,7 +33,7 @@ class DataViewController: UITableViewController
         var items = [BucketItem]()
         do
         {
-            if let filePath = Bundle.main.url(forResource: "Bucket18", withExtension: "csv")
+            if let filePath = Bundle.main.url(forResource: "bucket18", withExtension: "csv")
             {
                 let input = try String(contentsOf: filePath)
                 let bucketLines = input.components(separatedBy: "\n")
@@ -72,50 +73,5 @@ class DataViewController: UITableViewController
         return cell
     }
     
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
